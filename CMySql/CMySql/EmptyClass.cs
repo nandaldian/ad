bb3dd3a0aaadee034243202f2cd4c4b2a3021303
nandaldian <pkt1233123@gmail.com>
2017-10-04 12:48:54 +0200

    Añadidos CHolaMundo, dbprueba.sql, CMySql, MySql.Data.dll

---
+++ CMySql/CMySql/EmptyClass.cs
@@ -0,0 +1,77 @@
+﻿using Gtk;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+
+public partial class MainWindow : Gtk.Window
+//TODO Una clase singleton para la conexión
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+		treeView.AppendColumn("id", new CellRendererText(), "text", 0);
+		treeView.AppendColumn("nombre", new CellRendererText(), "text", 1);
+		ListStore listStore = new ListStore(typeof(long), typeof(string));
+		treeView.Model = listStore;
+		String command;
+		String id;
+		String nombre;
+
+
+		IDbConnection dbConnection = new MySqlConnection(
+			"Database=dbprueba;User Id=root;Password=sistemas"
+			);
+		IDbCommand cmd = dbConnection.CreateCommand();
+
+		try
+		{
+			dbConnection.Open();
+		}
+		catch (Exception)
+		{
+			Console.Clear();
+			//TODO Incluir los catch en ventanas a parte
+			//MessageDialog msgdiag = new MessageDialog (this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, "Error al conectar a la base de datos");
+
+		}
+		command = "select * from categoria";
+		cmd.CommandText = command;
+		IDataReader reader = cmd.ExecuteReader();
+		Boolean empty = true;
+
+		while (reader.Read())
+		{
+			id = reader[0].ToString();
+			nombre = reader[1].ToString();
+			//Console.WriteLine (id + " " + nombre);
+			listStore.AppendValues(Int64.Parse(id), nombre);
+			empty = false;
+		}
+		reader.Close();
+
+		if (empty)
+		{
+			//TODO Incluir los catch en ventanas a parte
+			//MessageDialog msgdiag = new MessageDialog 
+			//(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, 
+			//"La base de datos está vacía");
+		}
+
+
+
+
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+	protected void loadValues(IDbConnection dbConnection, ListStore listStore)
+	{
+
+	}
+
+
+}
---
+++ CMySql/CMySql/Program.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace CMySql
+{
+    class MainClass
+    {
+        private static IDbConnection connection;
+        public static void Main(string[] args)
+        {
+            //IDbConnection connection = new MySqlConnection(
+            string connectionString = "server = localhost;database = dbprueba;user = root;password=sistemas";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            nuevaCategoria();
+            //modificarCategoria();
+            leerCategorias();
+            connection.Close();
+        }
+
+
+        private static void leerCategorias()
+        {
+            IDbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = "select * from categoria order by id";
+            IDataReader dataReader = dbCommand.ExecuteReader();
+
+            while (dataReader.Read())
+                Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
+            dataReader.Close();
+        }
+
+
+
+
+        private static void nuevaCategoria()
+        {
+            IDbCommand dbCommand = connection.CreateCommand();
+            //dbCommand.CommandText = "insert into categoria (nombre) values ('categoria 4')";
+            dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+           
+            addParameter(dbCommand, "nombre", "categoria 6");
+            		    
+            int filasAfectadas = dbCommand.ExecuteNonQuery();
+		}
+
+		private static void addParameter(IDbCommand dbCommand, string name, object value)
+		{
+			IDbDataParameter dbDataParameter = dbCommand.CreateParameter();
+			dbDataParameter.ParameterName = name;
+			dbDataParameter.Value = value;
+			dbCommand.Parameters.Add(dbDataParameter);
+		}
+
+
+        private static void modificarCategoria(){
+			IDbCommand dbCommand = connection.CreateCommand();
+			dbCommand.CommandText = "update categoria set nombre='categoria44' where id=4";
+			int filasAfectadas = dbCommand.ExecuteNonQuery();
+        }
+
+        private static void eliminarCategoria()
+        {
+            IDbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = "delete from categoria where id=4";
+            int filasAfectadas = dbCommand.ExecuteNonQuery();
+        }
+    
+    }
+}
