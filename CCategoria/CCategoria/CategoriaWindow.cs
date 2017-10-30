894ab4acaf7cc26735d036cde14c7850053f612c
nandaldian <pkt1233123@gmail.com>
2017-10-30 13:22:32 +0100

    CCategoria refactorizadas MainWindow y CategoriaWindow usando CategoriaDao

--- CCategoria/CCategoria/CategoriaWindow.cs
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -7,35 +7,18 @@ namespace CCategoria
 
 	public partial class CategoriaWindow : Gtk.Window
 	{
-
-		object id;
-		public CategoriaWindow(object id) : base(Gtk.WindowType.Toplevel)
-		{
+        public CategoriaWindow(Categoria categoria) : base(Gtk.WindowType.Toplevel){
 			this.Build();
 
-            this.id = id;
-            Categoria categoria = CategoriaDao.Load(id);
-            entryNombre.Text = categoria.Nombre;
+			entryNombre.Text = categoria.Nombre ?? "";
 
 			saveAction.Activated += delegate {
-                categoria.Nombre = entryNombre.Text;
-                CategoriaDao.Save(categoria);
+				categoria.Nombre = entryNombre.Text;
+				CategoriaDao.Save(categoria);
 				Destroy();
 			};
-		}
+        }
 
-		public CategoriaWindow() : base(Gtk.WindowType.Toplevel)
-		{
-			this.Build();
-
-            Categoria categoria = new Categoria();
-
-
-			saveAction.Activated += delegate {
-                categoria.Nombre = entryNombre.Text;
-                CategoriaDao.Save(categoria);
-				Destroy();
-			};
-		}
-    }
+		
+	}
 }
