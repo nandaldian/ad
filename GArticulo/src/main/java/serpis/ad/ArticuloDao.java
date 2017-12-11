af7be7401268e55ef569e2f938cf997564cc35d7
nandaldian <pkt1233123@gmail.com>
2017-12-11 13:38:20 +0100

    Version final de GArticulo con todas las clases finalizadas

--- GArticulo/src/main/java/serpis/ad/ArticuloDao.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloDao.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArticuloDao {
 	private static Connection connection;
@@ -13,7 +16,6 @@ public class ArticuloDao {
 	private static PreparedStatement selectIdPreparedStatement;
 	private static String selectIdSql = 
 			"select * from articulo where id=?";
-	
 	public static Articulo get(long id) throws SQLException {
 		if (selectIdPreparedStatement == null)
 			selectIdPreparedStatement = connection.prepareStatement(selectIdSql);
@@ -29,32 +31,71 @@ public class ArticuloDao {
 	}
 	
 	private static String insertSql = 
-			"insert into articulo (nombre, precio, categoria) values (?, ?, ?)";
-		private static PreparedStatement insertPreparedStatement;
-		private static void insert(Articulo articulo) throws SQLException {
-			if (insertPreparedStatement == null)
-				insertPreparedStatement = connection.prepareStatement(insertSql);
-			insertPreparedStatement.setObject(1, articulo.getNombre());
-			insertPreparedStatement.setObject(2, articulo.getPrecio());
-			insertPreparedStatement.setObject(3, articulo.getCategoria());
-			int affected=insertPreparedStatement.executeUpdate();
-			System.out.println("\n Número de líneas afectadas: "+affected+"\n");
-		}
-		
-		private static PreparedStatement updatePreparedStatement;
-		private static String updateSql = 
-				"update articulo set nombre= ?, precio= ?, categoria= ? where id= ?";
-		private static void update(Articulo articulo) throws SQLException {
-			if (updatePreparedStatement == null)
-				updatePreparedStatement = connection.prepareStatement(updateSql);
-			updatePreparedStatement.setObject(1, articulo.getNombre());
-			updatePreparedStatement.setObject(2, articulo.getPrecio());
-			updatePreparedStatement.setObject(3, articulo.getCategoria());
-			updatePreparedStatement.setObject(4, articulo.getId());
-			int affected= updatePreparedStatement.executeUpdate();
-			System.out.println("\n Número de líneas afectadas: "+affected+"\n");
-		}
-		
-		
+		"insert into articulo (nombre, precio, categoria) values (?, ?, ?)";
+	private static PreparedStatement insertPreparedStatement;
+	private static void insert(Articulo articulo) throws SQLException {
+		if (insertPreparedStatement == null)
+			insertPreparedStatement = connection.prepareStatement(insertSql);
+		insertPreparedStatement.setObject(1, articulo.getNombre());
+		insertPreparedStatement.setObject(2, articulo.getPrecio());
+		insertPreparedStatement.setObject(3, articulo.getCategoria());
+		int affected=insertPreparedStatement.executeUpdate();
+		System.out.println("\n Número de líneas afectadas: "+affected+"\n");
+	}
+	private static PreparedStatement updatePreparedStatement;
+	private static String updateSql = 
+			"update articulo set nombre= ?, precio= ?, categoria= ? where id= ?";
+	private static void update(Articulo articulo) throws SQLException {
+		if (updatePreparedStatement == null)
+			updatePreparedStatement = connection.prepareStatement(updateSql);
+		updatePreparedStatement.setObject(1, articulo.getNombre());
+		updatePreparedStatement.setObject(2, articulo.getPrecio());
+		updatePreparedStatement.setObject(3, articulo.getCategoria());
+		updatePreparedStatement.setObject(4, articulo.getId());
+		int affected= updatePreparedStatement.executeUpdate();
+		System.out.println("\n Número de líneas afectadas: "+affected+"\n");
+	}
 	
+	public static void save(Articulo articulo) throws SQLException {
+		if (articulo.getId() == 0)
+			insert(articulo);
+		else
+			update(articulo);
+	}
+	
+	private static PreparedStatement deletePreparedStatement;
+	private static String deleteSql = 
+			"delete from articulo where id= ?";
+	public static void delete(long id) throws SQLException {
+		if (deletePreparedStatement == null)
+			deletePreparedStatement = connection.prepareStatement(deleteSql);
+		deletePreparedStatement.setObject(1, id);
+		int affected= deletePreparedStatement.executeUpdate();
+		System.out.println("\n Número de líneas afectadas: "+affected+"\n");
+		
+		
+	}
+	
+	private static String selectSql = 
+			"select * from articulo";
+	public static List<Articulo> getList() throws SQLException {
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery(selectSql);
+		List<Articulo> articulos = new ArrayList<>();
+		while (resultSet.next()){
+			Articulo articulo = new Articulo();
+			articulo.setId(resultSet.getLong("id"));
+			articulo.setNombre(resultSet.getString("nombre"));
+			articulo.setPrecio(resultSet.getBigDecimal("precio"));
+			articulo.setCategoria(resultSet.getLong("categoria"));
+			articulos.add(articulo);
+		}
+		return articulos;
+	}
+	
+	public static void close() throws SQLException {
+		if (insertPreparedStatement != null)
+			insertPreparedStatement.close();
+	}
+
 }
--- GArticulo/src/main/java/serpis/ad/ArticuloMain.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloMain.java
@@ -1,69 +1,150 @@
 package serpis.ad;
 
-import java.lang.reflect.Field;
-import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-
-
 public class ArticuloMain {
-	//Pruebas de clase	
-	public enum Option {Salir, Nuevo, Editar, Eliminar, Consultar, Listar};
+	
 	private static Scanner scanner = new Scanner(System.in);
-	public static class Articulo {
+	
+	private static class Action {
+		private String label;
+		private Runnable runnable;
 		
-		private long id;
-		private String nombre;
-		private BigDecimal precio;
-		private long categoria;
+		public Action(String label, Runnable runnable) {
+			this.label = label;
+			this.runnable = runnable;
+		}
+		public String getLabel() {
+			return label;
+		}
+		public void execute() {
+			runnable.run();
+		}
 	}
 	
-	
-	public static void main(String[] args) {
-		scan(Option.class);
-//		pruebaLong("introduce un numero: ");
+	private static class Menu {
+		private List<Action> actions = new ArrayList<>();
+		public Menu add(Action action) {
+			actions.add(action);
+			return this;
+		}
 		
-		Runnable runnable = () -> nuevo();
-		runnable.run();
+		private String getOptions() {
+			String options = "";
+			for (int index = 0; index < actions.size(); index ++) 
+				options = options + index;
+			return "[" + options + "]";
+		}
 		
-		showFields(Articulo.class);
-		
-	}
-//		public static long pruebaLong(String prueba) {
-//			while (true) 
-//				try {		
-//					System.out.println(prueba);
-//					String line = scanner.nextLine();
-//					return Long.parseLong(line);
-//				} catch (NumberFormatException ex) {
-//					System.out.println("Por favor, sólo números. Vuelve a introducir");
-//					
-//				}
-//		}
-		
-		public static <T extends Enum<T>> T scan(Class<T> enumType) {
-			T[] constants = enumType.getEnumConstants();
-			for(int index = 0; index < constants.length; index++)
-				System.out.printf("%s - %s\n", index, constants[index]);
-			String options = String.format("^[0-%s]$", constants.length -1);
+		private int getOption() {
+			String options = getOptions();
 			while (true) {
-				System.out.println("Elige una opcion: ");
-				String line = scanner.nextLine();
-				if (line.matches(options))
-					return constants[Integer.parseInt(line)];
-				System.out.println("Opcion invalida. Vuelve a introducir.");	
-				}
-		}
-		public static void nuevo(){
-			//TODO implementar
-		}
-		
-		
-		private static void showFields(Class<?> type) {
-			for (Field field : type.getDeclaredFields() )
-				System.out.printf("%s - %s", field.getName(), field.getType().getName());
+				for (int index = 0; index < actions.size(); index ++) 
+					System.out.printf("%d %s\n", index, actions.get(index).label);
+				
+				System.out.print("Introduce opción: ");
+				String option = scanner.nextLine();
+				if (option.matches(options))
+					return Integer.parseInt(option);
+			}
 		}
 		
+		public void show() {
+			while (true) {
+				int option = getOption();
+				actions.get(option).execute();
+				if (option == 0)
+					return;
+			}
+		}
+	}
+	
+
+	private static void salir() {
+		System.out.println("Fin del Programa.......");
+	}
+
+	private static void nuevo() {
+		Articulo articulo = new Articulo();
+		ArticuloView.get(articulo);
+		try  {
+			ArticuloDao.save(articulo);
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+	}
+	
+	private static void modificar() {
+		Articulo articulo = new Articulo();
+		long id = ArticuloView.getId();
+		articulo.setId(id);
+		ArticuloView.get(articulo);
+		try {
+			ArticuloDao.save(articulo);
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+	}
+
+	private static void eliminar() {
+		long id = ArticuloView.getId();
+		try{
+		ArticuloDao.delete(id);
+		}catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+	}
+
+	private static void consultar() {
+		long id = ArticuloView.getId();
+		Articulo articulo;
+		try {
+			articulo = ArticuloDao.get(id);
+			ArticuloView.show(articulo);
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+		
+	}
+	
+	private static void listarTodos() {
+		try {
+			List<Articulo> articulos = ArticuloDao.getList(); 
+			ArticuloView.show(articulos);
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+		}
+	}
+
+	private static Connection connection;
+	private static void init() throws SQLException {
+		connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		ArticuloDao.init(connection);
+	}
+	
+	private static void close() throws SQLException {
+		ArticuloDao.close();
+		connection.close();
+	}
+
+	public static void main(String[] args) throws SQLException {
+		init();
+		Menu menu = new Menu()
+			.add(new Action("Salir del Programa", ()->salir()))
+			.add(new Action("Nuevo", ()->nuevo()))
+			.add(new Action("Modificar", ()->modificar()))
+			.add(new Action("Eliminar", ()->eliminar()))
+			.add(new Action("Consultar", ()->consultar()))
+			.add(new Action("Listar todos", ()->listarTodos()))
+		;
+		menu.show();
+		close();
+	}
+
 }
-
-
